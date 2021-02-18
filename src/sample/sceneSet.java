f4ab88ca4470bc851f1c8f53a4a5870276ba0840
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-02-18 01:06:50 +0100

    cleaning up the code

--- src/sample/sceneSet.java
+++ src/sample/sceneSet.java
@@ -1,30 +1,46 @@
 package sample;
 
+/*
+=================================================================================
+	IMPORTS:
+=================================================================================
+*/
+
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+
+
+/*
+=================================================================================
+	MAIN PROGRAM CODE:
+=================================================================================
+*/
+
 public class sceneSet extends Application {
 
-	int win_width = 600, win_height = 550;
+	int win_width = 600, win_height = 550;		// main window (stage) parameters (window width and window height)
+
+
 
 	@Override
-	public void start(Stage stage) throws Exception {
+	public void start(Stage stage) throws Exception {		//starting program with default stage(s)...
 
 
 
 	/*
 	=================================================================================
-	OBJECTS:
+		OBJECTS:
 	=================================================================================
 	*/
 
-		Label l0 = new Label("Hello there...");
+		Label l0 = new Label("Hello there...");		//creating labels that will be shown on stage
 		Label l1 = new Label("Hello...");
 
-		VBox vboxObj0 = new VBox(l0);
+		VBox vboxObj0 = new VBox(l0);		// creating a VBox
 		VBox vboxObj1 = new VBox(l1);
 
 		Scene s0 = new Scene(vboxObj0);		//creating a scene
@@ -35,11 +51,11 @@ public class sceneSet extends Application {
 
 	/*
 	=================================================================================
-	MAIN:
+		MAIN:
 	=================================================================================
 	*/
 
-		st0.setWidth(win_width);
+		st0.setWidth(win_width);		//setting up stage parameters (window size and title)
 		st0.setHeight(win_height);
 		st0.setTitle("My stage");
 
@@ -48,8 +64,6 @@ public class sceneSet extends Application {
 		st0.show();					//showing st0 stage..
 
 	}
-
-
 
 
 	/*
@@ -63,15 +77,20 @@ public class sceneSet extends Application {
 		System.out.println("Application opened");
 	}
 
+
+
 	@Override
 	public void stop() throws Exception {
 		System.out.println("Application closed");
 	}
 
+
+
 	public static void main(String[] args) {
 		launch();
 	}
 }
+
 
 
 /*
