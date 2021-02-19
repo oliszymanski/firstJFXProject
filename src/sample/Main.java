0bb793b6330797495563adc0dfb904351d6c0eab
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-02-19 01:15:33 +0100

    cleaning up Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,56 +1,90 @@
 package sample;
 
+
+
+/*
+=================================================================================
+	IMPORTS:
+=================================================================================
+*/
+
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+
+
+/*
+=================================================================================
+	MAIN PROGRAM:
+=================================================================================
+*/
+
 public class Main extends Application {
+
+    int sc_width = 400;     // setting window size (width and height)
+    int sc_height = 200;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        //Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
 
-        int sc_width = 400;
-        int sc_height = 200;
-//      Button button = new Button("Press me");
-        primaryStage.setTitle("Primary window");    //call first window "Primary window"
 
-        //some other code that displays the screen...
+
+        /*
+        =================================================================================
+	        OBJECTS:
+        =================================================================================
+        */
+
         Label lbl = new Label("Hello world!");    //Label(s_sentence);
         Scene s1 = new Scene(lbl, sc_width, sc_height);    //Scene(n objects in scene, scene_width, scene_height)
-        primaryStage.setScene(s1);
-        primaryStage.show();
+        Button button = new Button("Press me");
+
+        Stage stage2 = new Stage();         //creating new stage
 
 
 
+        /*
+	    =================================================================================
+		    MAIN:
+	    =================================================================================
+	    */
 
-        //create a second window that will show up with the first one:
+        primaryStage.setTitle("Primary window");        //stage titles
+        stage2.setTitle("Secondary window");
 
+        primaryStage.setScene(s1);      //stage scenes
 
-        /*Stage stage2 = new Stage();
-        stage2.setTitle("Secondary window");    //call second window "Secondary window"
-        stage2.show();*/
+        primaryStage.show();            //displaying stages on screen...
+
+        stage2.show();
     }
 
 
-    //TODO: clean the whole code and commit...
+
+    /*
+	 =================================================================================
+	 	RUN APPLICATION:
+	 =================================================================================
+	*/
 
     @Override
     public void init() throws Exception{
         System.out.println("AppState> Opened");
     }
 
+
+
     @Override
     public void stop() throws Exception{
         System.out.println("AppState> Closed");
     }
 
+
+
     public static void main(String[] args) {
-        //launching the app:
         launch();
     }
 }
