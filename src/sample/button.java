1554ad8223800042d21867b3626bf276cb5f6a20
Oliwier Szymanski <oliwier.szymanski112@gmail.com>
2021-03-04 00:18:29 +0100

    adding the button.java file

---
+++ src/sample/button.java
@@ -0,0 +1,80 @@
+package sample;
+
+
+/*
+=================================================================================
+	IMPORTS:
+=================================================================================
+*/
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+
+
+/*
+=================================================================================
+	MAIN PROGRAM CODE:
+=================================================================================
+*/
+
+public class button extends Application {
+
+	int win_width = 600, win_height = 550;		// main window (stage) parameters (window width and window height)
+
+	@Override
+	public void start(Stage stage) throws Exception {
+
+
+
+	/*
+	=================================================================================
+		OBJECTS:
+	=================================================================================
+	*/
+
+		Button btn = new Button("Click me");	//creating a button
+		Scene sc = new Scene(btn);			//creating a scene
+
+
+	/*
+	=================================================================================
+		MAIN:
+	=================================================================================
+	*/
+
+		stage.setTitle("new");			//setting upi the main
+		stage.setMaxWidth(win_width);
+		stage.setMaxHeight(win_height);
+		//stage.setScene();
+		stage.show();
+
+	}
+
+
+	/*
+	 =================================================================================
+	 	RUN APPLICATION:
+	 =================================================================================
+	*/
+
+	@Override
+	public void init() throws Exception {
+		System.out.println("Application opened");
+	}
+
+
+
+	@Override
+	public void stop() throws Exception {
+		System.out.println("Application closed");
+	}
+
+
+
+	public static void main(String[] args) {
+		launch();
+	}
+}
